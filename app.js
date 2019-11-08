a04a3aa033fec04e9507e19f41ce3954edfda728
Ted Stark <info4tws@gmail.com>
2019-11-08 00:18:19 -0700

    dotenv changes

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
+const http = require('http');
+const env = process.env;
+
+// To read enviornmental variables
+require('dotenv').config(); //dotenv
+
+//For Timestamp messages in console
+require('console-stamp')(console, 'HH:MM:ss');
+
+const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
+    let message = 'It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+        monkey = env.MONKEY + '\n',
+        elephant = env.ELEPHANT + '\n',
+        lion = env.LION + '\n',
+        response = [message, version, monkey, elephant, lion].join('\n');
     res.end(response);
 });
 server.listen();
