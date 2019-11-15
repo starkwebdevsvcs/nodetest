9ceb8fc3c9dc305cc2693811c953bd64ebb7b436
Stark Web Development Services <ted@starkweb.dev>
2019-11-15 01:13:36 -0500

    Addition of yenv

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     let message = 'It works!!!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
-        monkey = env.MONKEY + '\n',
-        elephant = env.ELEPHANT + '\n',
-        lion = env.LION + '\n',
-        response = [message, version, monkey, elephant, lion].join('\n');
+//        monkey = env.MONKEY + '\n',
+//        elephant = env.ELEPHANT + '\n',
+//        lion = env.LION + '\n',
+        mongo1 = env.MONGODB_STRING + '\n',
+        mongo2 = env.MONGODB_STRING2 + '\n',
+        mongo3 = env.MONGODB_STRING3 + '\n',
+//        response = [message, version, monkey, elephant, lion, mongo1].join('\n');
+        response = [message, mongo1, mongo2, mongo3].join('\n');
     res.end(response);
 });
 server.listen();
